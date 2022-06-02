70878990a90604e773e542cfbda201fdb6da8d75
Dmitriy Stolyarov <de.stolyarov@gmail.com>
2022-06-03 01:33:17 +0500

    Problem 25 solved.

--- ProjectEulerSolution/Program.cs
+++ ProjectEulerSolution/Program.cs
@@ -15,6 +15,6 @@ internal class Program
 
     private static IProblem CreateProblem()
     {
-        return new BenchmarkDecorator(new Problem24());
+        return new BenchmarkDecorator(new Problem25());
     }
 }
